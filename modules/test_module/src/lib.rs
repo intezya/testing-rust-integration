1d3f2bfb92b32e7e249a3a0751225dd426e6c4d2
intezya <intezya@gmail.com>
2024-08-02 23:23:38 +0400

    Add functions to lib.rs

--- modules/test_module/src/lib.rs
+++ modules/test_module/src/lib.rs
@@ -1,3 +1,6 @@
+mod factorial;
+mod fibonacci;
+
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -10,5 +13,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn test_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(factorial::py_factorial, m)?)?;
+    m.add_function(wrap_pyfunction!(fibonacci::py_fibonacci, m)?)?;
     Ok(())
 }
