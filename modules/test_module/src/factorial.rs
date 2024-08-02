a9d99a4bc469d9bcd8e8df0aa4250bd43dd26bc0
intezya <intezya@gmail.com>
2024-08-02 23:32:53 +0400

    Update lib version to 0.2.1

--- modules/test_module/src/factorial.rs
+++ modules/test_module/src/factorial.rs
@@ -1,13 +1,13 @@
-use pyo3::prelude::pyfunction;
-use pyo3::prelude::PyResult;
+use num_bigint::BigUint;
+use pyo3::{pyfunction, PyResult};
 
-
-fn factorial(n: u32) -> u32 {
+fn factorial(n: u32) -> BigUint {
     return (1..=n).product()
 }
 
 
 #[pyfunction]
-pub fn py_factorial(n: u32) -> PyResult<u32> {
-    Ok(factorial(n))
+pub fn py_factorial(n: u32) -> PyResult<String> {
+    let result = factorial(n);
+    Ok(result.to_str_radix(36))
 }
