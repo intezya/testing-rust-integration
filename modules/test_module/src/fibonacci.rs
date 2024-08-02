d8b1f73609a6d8d17a83ae7a579ec209cfa5ac0d
intezya <intezya@gmail.com>
2024-08-02 23:23:21 +0400

    Upd fibonacci.rs

--- modules/test_module/src/fibonacci.rs
+++ modules/test_module/src/fibonacci.rs
@@ -1,7 +1,16 @@
-pub fn fibonacci(n: u32) -> u32 {
+use pyo3::prelude::pyfunction;
+use pyo3::prelude::PyResult;
+
+
+fn fibonacci(n: u32) -> u32 {
     match n {
         0 => 0,
         1 => 1,
         _ => fibonacci(n - 1) + fibonacci(n - 2),
     }
+}
+
+#[pyfunction]
+pub fn py_fibonacci(n: u32) -> PyResult<u32> {
+    Ok(fibonacci(n))
 }
